b196eb0245022a3473dd5fd898faffb6612d3ec6
darwinfroese <froese.darwin@gmail.com>
2015-12-28 20:57:38 -0600

    Actually added the file for using Document in HashSet.

---
+++ MyDocumanager/DocumentComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace MyDocumanager
+{
+  class DocumentComparer : IEqualityComparer<Document>
+  {
+    public bool Equals(Document x, Document y)
+    {
+      bool isEqual = x.Equals(y);
+
+      return isEqual;
+    }
+
+    public int GetHashCode(Document obj)
+    {
+      return obj.GenerateHashCode();
+    }
+  }
+}
